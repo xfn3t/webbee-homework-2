ed1ce43fca27a194f6c1078c4808e14e9fcb2e25
xfn3t <n.semenuta4830@gmail.com>
2025-07-16 19:15:52 +0300

    feat(country-dto): define CountryDto for API

---
+++ contractor/src/main/java/ru/homework/domain/reference/country/dto/CountryDto.java
@@ -0,0 +1,11 @@
+package ru.homework.domain.reference.country.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CountryDto {
+	private String id;
+	private String name;
+}
